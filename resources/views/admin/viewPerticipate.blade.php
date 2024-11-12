289f96f4383bb2a329625698d94f54488e66aee7
codergaint <71995368+codergaint@users.noreply.github.com>
2024-11-12 07:43:42 +0600

    Update viewPerticipate.blade.php

--- resources/views/admin/viewPerticipate.blade.php
+++ resources/views/admin/viewPerticipate.blade.php
@@ -196,7 +196,7 @@ Verified Student
                                     </div>
                                     <!--end row-->
                                     <div class="row">
-                                        <div class="col-12">
+                                        <div class="col-12 my-2">
                                             @if($student->status == 'PendingVerify')
                                             <a href="{{ route('acceptRegister',['id'=>$student->id]) }}" onclick="alert('Are you sure, you verify the data?')" class="btn btn-success my-1">
                                                     <i class="fa-solid fa-badge-check"></i> Accept & Confirm
@@ -205,6 +205,8 @@ Verified Student
                                                     <i class="fa-solid fa-circle-xmark"></i> Deny & Reject
                                                 </a> 
                                             @endif
+                                        </div>
+                                        <div class="col-12 my-2">
                                             <a href="{{ route('pendingList') }}" class="btn btn-warning fw-bold"><i class="fa-sharp fa-regular fa-calendar-clock"></i> Pending List</a>
                                             <a href="{{ route('verifiedList') }}" class="btn btn-success fw-bold"><i class="fa-duotone fa-solid fa-check-to-slot"></i> Verified List</a>
                                             <a href="{{ route('rejectedList') }}" class="btn btn-danger fw-bold"><i class="fa-regular fa-shuffle"></i> Rejected List</a>
