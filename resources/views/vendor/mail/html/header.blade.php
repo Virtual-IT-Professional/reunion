57c036a9c85a4a02a41e4efcfa01105a2de47876
codergaint <71995368+codergaint@users.noreply.github.com>
2024-11-17 10:08:41 +0600

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -5,8 +5,7 @@
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
-<img src="{{ $url }}/{{ asset('/public/admin/velzon/html/default/assets/images/logo.png') }}" class="logo" alt="{{ $slot }}">
-{{ $slot }}
+<img src="{{ $url }}/public/admin/velzon/html/default/assets/images/logo.png" class="logo" alt="{{ $slot }}">
 @endif
 </a>
 </td>
