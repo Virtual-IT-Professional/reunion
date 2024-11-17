6237eacb5f905133cbf700d794e1f32dc51d376f
codergaint <71995368+codergaint@users.noreply.github.com>
2024-11-17 10:38:29 +0600

    Update thankYouPage.blade.php

--- resources/views/front/thankYouPage.blade.php
+++ resources/views/front/thankYouPage.blade.php
@@ -1,7 +1,7 @@
 @extends('front.include')
 @section('frontBody')
 <div class="container p-4 mt-4">
-    <div class="row align-items-center mt-5">
+    <div class="row align-items-center mt-4">
         <div class="col-11 mx-auto">
             <div class="card card-body shadow p-4">
                 @if(Session::has('error'))
