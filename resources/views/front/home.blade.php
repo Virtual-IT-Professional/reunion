fda53ef375ebbdff2ee4803d2fec0ecf23b34a77
codergaint <71995368+codergaint@users.noreply.github.com>
2024-11-29 07:07:03 +0600

    Update home.blade.php

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -114,7 +114,7 @@
                     $verifiedList = \App\Models\studentRegister::where(['status'=>'Verified'])->orderby('id','DESC')->get();
                     $verifiedGuest = \App\Models\geustRegister::where(['status'=>'Verified'])->orderby('id','DESC')->get();
 
-                    $todayRegister = \App\Models\studentRegister::whereDay('created_at',now())->where(['status'=>'Verified'])->get();
+                    $todayRegister = \App\Models\studentRegister::whereDay('updated_at',now())->where(['status'=>'Verified'])->get();
 
                     $totalRegister = count($verifiedList)+count($verifiedGuest);
                 @endphp
